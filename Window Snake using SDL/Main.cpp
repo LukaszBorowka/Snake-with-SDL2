230bbd61d4fa1b105f7d15d1274a4ddb93461c11
LukaszBorowka <lukasz.borowka19@icloud.com>
2024-05-18 21:25:11 +0200

    class Game - constructor and destructor working

--- Window Snake using SDL/Main.cpp
+++ Window Snake using SDL/Main.cpp
@@ -1,56 +1,84 @@
-/*This source code copyrighted by Lazy Foo' Productions 2004-2024
-and may not be redistributed without written permission.*/
-
-//Using SDL and standard IO
+#include <iostream>
+#include <vector>
 #include <SDL.h>
-#include <stdio.h>
 
-//Screen dimension constants
 const int SCREEN_WIDTH = 640;
 const int SCREEN_HEIGHT = 480;
 
+struct SnakePart {
+	int posX;
+	int posY;
+	Uint32 col;
+};
+
+class Game {
+private:
+	SDL_Window* window = NULL;
+	SDL_Surface* surface = NULL;
+
+	SDL_Event event;
+
+	const int gameWidth = 500;
+	const int gameHeight = 500;
+
+	std::vector<SnakePart> snakePart = {};
+
+	bool isRunning = false;
+
+public:
+	Game() {
+		if (SDL_Init(SDL_INIT_VIDEO) < 0) {
+			std::cerr << "SDL could not be initialized. SDL error:\n" << SDL_GetError();
+			exit;
+		}
+
+		this->window = SDL_CreateWindow(
+			"Snake Game in c++ & SDL2!",
+			SDL_WINDOWPOS_UNDEFINED,
+			SDL_WINDOWPOS_UNDEFINED,
+			this->gameWidth,
+			this->gameHeight,
+			SDL_WINDOW_SHOWN);
+		if (this->window == NULL) {
+			std::cerr << "Window could not be created. SDL error:\n" << SDL_GetError();
+			exit;
+		}
+
+		this->surface = SDL_GetWindowSurface(this->window);
+		if (this->surface == NULL) {
+			std::cerr << "Surface error. SDL error:\n" << SDL_GetError();
+			exit;
+		}
+
+		SDL_FillRect(this->surface, NULL, SDL_MapRGB(this->surface->format, 0xFF, 0xFF, 0xFF));
+
+		SDL_UpdateWindowSurface(this->window);
+
+		this->isRunning = true;
+		this->loop();
+	}
+
+	~Game() {
+		SDL_DestroyWindow(window);
+		SDL_Quit();
+	}
+
+	void loop() {
+		while (isRunning) {
+
+
+			while (SDL_PollEvent(&event)) {
+				if (event.type == SDL_QUIT) isRunning = false;
+			}
+		}
+	}
+};
+
 int main(int argc, char* args[])
 {
-	//The window we'll be rendering to
-	SDL_Window* window = NULL;
-
-	//The surface contained by the window
-	SDL_Surface* screenSurface = NULL;
-
-	//Initialize SDL
-	if (SDL_Init(SDL_INIT_VIDEO) < 0)
 	{
-		printf("SDL could not initialize! SDL_Error: %s\n", SDL_GetError());
-	}
-	else
-	{
-		//Create window
-		window = SDL_CreateWindow("SDL Tutorial", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN);
-		if (window == NULL)
-		{
-			printf("Window could not be created! SDL_Error: %s\n", SDL_GetError());
-		}
-		else
-		{
-			//Get window surface
-			screenSurface = SDL_GetWindowSurface(window);
-
-			//Fill the surface white
-			SDL_FillRect(screenSurface, NULL, SDL_MapRGB(screenSurface->format, 0xFF, 0xFF, 0xFF));
-
-			//Update the surface
-			SDL_UpdateWindowSurface(window);
-
-			//Hack to get window to stay up
-			SDL_Event e; bool quit = false; while (quit == false) { while (SDL_PollEvent(&e)) { if (e.type == SDL_QUIT) quit = true; } }
-		}
-	}
-
-	//Destroy window
-	SDL_DestroyWindow(window);
-
-	//Quit SDL subsystems
-	SDL_Quit();
+		Game* game = new Game();
+	} // autmatically calls Game destructor ~Game()
 
 	return 0;
 }
